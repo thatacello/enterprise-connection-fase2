802d5944ef83df5ac85299e670398fd4f08c4267
thatacello <farias.thais@uol.com.br>
2022-04-12 22:49:24 -0300

    add tokens and normalize

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { NormalizeStyles } from './shared/NormalizeStyles';
+export function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NormalizeStyles />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/detalhes/:id" exact element={''} />
+          <Route path="/home" exact element={''} />
+          <Route path="*">Página não encontrada</Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
-
-export default App;
